b5ab0d81c7a09e5216599aeb71f8aa83a1b22342
Will Wheeler <willwheelera@gmail.com>
2018-08-06 09:56:45 -0500

    clean up file rundft

--- rundft.py
+++ rundft.py
@@ -4,11 +4,11 @@ Some simple tests to check that autogen runs without erroring out.
 Run repeatedly to check if runner is also checking queued items correctly.
 '''
 
-from autogenv2 import autopyscf,autorunner,crystal
-from crystal import CrystalWriter
+#from autogenv2 import autopyscf,autorunner,crystal
+#from crystal import CrystalWriter
 from autopyscf import PySCFWriter,PySCFPBCWriter,dm_set_spins
 from pyscfmanager import PySCFManager
-from crystalmanager import CrystalManager
+#from crystalmanager import CrystalManager
 from qwalkmanager import QWalkManager
 from autorunner import PySCFRunnerLocal,PySCFRunnerPBS,RunnerPBS
 from variance import VarianceWriter,VarianceReader
@@ -19,13 +19,17 @@ import sys
 
 
 
-def si_pyscf_test():
+def si_pyscf():
   ''' Simple tests that check PBC is working Crystal, and that QMC can be performed on the result.'''
   jobs=[]
 
   pwriter=PySCFPBCWriter({
       'gmesh':[16,16,16],
-      'cif':open('si.cif','r').read()
+      'cif':open('si.cif','r').read(),
+      'dft':'pbe0',
+      'method':'ROKS',
+      'basis':'bfd_vtz',
+      'ecp':'bfd'
     })
   pman=PySCFManager(
       name='scf',
@@ -34,7 +38,7 @@ def si_pyscf_test():
       runner=PySCFRunnerPBS(
           queue='secondary',
           nn=1,
-          #np=16,
+          np='allprocs',
           walltime='4:00:00'
         )
     )
@@ -67,13 +71,13 @@ def si_pyscf_test():
   return jobs
 
 
-def run_tests():
+def run():
   ''' Choose which tests to run and execute `nextstep()`.'''
   jobs=[]
-  jobs+=si_pyscf_test()
+  jobs+=si_pyscf()
 
   for job in jobs:
     job.nextstep()
 
 if __name__=='__main__':
-  run_tests()
+  run()
